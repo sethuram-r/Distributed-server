a094dde018d90e5f3f97e5d38233cfaf881eac37
Sethuram Ramalinga Reddy <sethuram500@gmail.com>
2017-12-10 13:46:32 +0000

    Update place_holder.py

--- place_holder.py
+++ place_holder.py
@@ -13,3 +13,8 @@ lock_attempts = 1
 requested_lock = "Requested_Lock:{}\nClient:{}"
 requested_use = "Requested_Use:{}\nClient:{}"
 requested_unlock = "Requested_Unlock:{}\nClient:{}"
+
+
+read_file = "Read_File:{}\nClient:{}"
+delete_file = "Delete_File:{}\nClient:{}"
+return_file_data = "Return_File_Data\n"
